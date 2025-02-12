79f11f4fc5b6e8ecef3a987a517c36ae3c08bb76
Kawya Awarjana <kawya.evotech@gmail.com>
2025-02-12 18:29:47 +0530

    Kawya

---
+++ header/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skill-y</title>
+    <link rel="icon" href="../resources/img/" />
+    <link rel="stylesheet" href="../header/header.css">
+</head>
+
+<body>
+
+    <section class="header--sec1">
+        <div class="header--sec1--div1">
+            <div class="header--sec1--div2">
+                <div class="header--sec1--div3">
+                    <img src="../resources/img/header/logo.png" alt="logo" class="header--logo--lg">
+                </div>
+                <div class="header--sec1--div4">
+                    <div class="header--sec1--div5">
+
+                    </div>
+                    <div class="header--sec1--div6">
+
+                    </div>
+                    <div class="header--sec1--div7">
+
+                    </div>
+                </div>
+                <div class="header--sec1--div8">
+                    <div class="header--sec1--div9">
+
+                    </div>
+                </div>
+            </div>
+            <div class="header--sec1--div10">
+
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
