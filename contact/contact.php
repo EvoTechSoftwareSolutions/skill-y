c90f9708f54803c7ec882cdabf4781c174bfbdde
Kawya Awarjana <kawya.evotech@gmail.com>
2025-02-15 20:57:25 +0530

    Kawya

--- contact/contact.php
+++ contact/contact.php
@@ -18,11 +18,11 @@
     <!-- Header -->
 
     <div class="contact--bgimg">
-        <!-- <img src="../resources/img/contact/Rectangle2.png" class="contact--bg1" /> -->
+        <img src="../resources/img/contact/Rectangle3.png" class="contact--bg1" />
     </div>
 
     <div class="contact--bgimg1">
-        <!-- <img src="../resources/img/contact/Group2.png" class="contact--bg2" /> -->
+        <img src="../resources/img/contact/Group3.png" class="contact--bg2" />
     </div>
 
     <section class="contact--sec1">
@@ -95,6 +95,212 @@
             </div>
         </div>
     </section>
+
+    <br />
+
+    <section class="contact--sec1">
+        <div class="contact--sec1--div1">
+            <span class="contact--sec1--span1">Skill-y /<span class="contact--sec1--span2"> Contact Us</span></span>
+        </div>
+    </section>
+
+    <section class="contact--sec2">
+        <div class="contact--sec2--div1">
+            <span class="contact--sec2--span1">Contact Us</span>
+            <span class="contact--sec2--span2">Get In<span class="contact--sec2--span3"> Touch</span></span>
+        </div>
+    </section>
+
+
+
+
+
+
+
+    <section class="contact--sec5">
+        <div class="contact--sec5--div1">
+            <div class="contact--sec5--div2">
+                <div class="contact--sec5--div3">
+                    <div class="contact--sec5--div4">
+                        <div class="contact--sec5--div5">
+                            <div class="contact--sec5--div6">
+                                <div class="contact--sec5--div7">
+                                    <input type="text" class="contact--txtfield" id="fname" placeholder="First Name">
+                                </div>
+                                <div class="contact--sec5--div7">
+                                    <input type="text" class="contact--txtfield" id="lname" placeholder="Last Name">
+                                </div>
+                            </div>
+                            <div class="contact--sec5--div6">
+                                <div class="contact--sec5--div7">
+                                    <input type="text" class="contact--txtfield" id="phone" maxlength="16" placeholder="Phone Number">
+                                </div>
+                                <div class="contact--sec5--div7">
+                                    <input type="text" class="contact--txtfield" id="email" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="contact--sec5--div8">
+                                <input type="text" class="contact--txtfield" id="subject" placeholder="Subject">
+                            </div>
+                            <div class="contact--sec5--div8">
+                                <textarea class="contact--txtarea" id="message" placeholder="Message"></textarea>
+                            </div>
+                            <div class="contact--sec5--divBtn">
+                                <button class="contatct--button" id="sub_btn" onclick="send();">Send Message</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact--sec5--div9">
+                        <div class="contact--sec5--div10">
+                            <div class="contact--sec5--div11">
+                                <span class="contatct--sec5--txt2">
+                                    Your dream journey begins here! At KHK Tours, we believe that every traveler deserves a seamless and unforgettable experience, and we're here to make that happen. Whether you're ready to book your next adventure, seeking advice on destinations, or just have a few questions about our services, our dedicated team is here to assist you every step of the way.
+                                    <br />
+                                    From choosing the perfect itinerary to answering all your queries, we take pride in providing personalized guidance that caters to your unique travel needs. No question is too small, and no detail is overlooked – we’re committed to ensuring your trip is as smooth and enjoyable as possible.Reach out to us through your preferred method – call, email, or message – and let’s start crafting an adventure tailored just for you. With KHK Tours, your journey is more than just a trip; it’s a collection of unforgettable memories waiting to unfold. We look forward to hearing from you and making your travel dreams a reality!
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="contatct--verticle--hr"></div> -->
+                </div>
+            </div>
+            <div class="contact--sec5--div12">
+                <div class="contact--sec5--div13">
+                    <div class="contact--sec5--div17">
+                        <div class="contact--sec5--div14" style="background-color: #4D6D1D;">
+                            <div class="contact--sec5--div15">
+                                <table>
+                                    <tr>
+                                        <td><img src="../resources/img/contact/icon1.png" class="table--img1" /></td>
+                                        <td class="table--span1">Contact Number</td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="contact--sec5--div16">
+                                <span class="sec5--iconspan">+64 27 329 6864</span>
+                            </div>
+                        </div>
+                        <div class="contact--sec5--div14" style="background-color: #669127;">
+                            <div class="contact--sec5--div15">
+                                <table>
+                                    <tr>
+                                        <td class="table--img1"><img src="../resources/img/contact/icon2.png" class="table--img1" /></td>
+                                        <td class="table--span1">Email Address</td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="contact--sec5--div16">
+                                <span class="sec5--iconspan">info@skilly.co.nz</span>
+                            </div>
+                        </div>
+                        <div class="contact--sec5--div14" style="background-color: #80B531;">
+                            <div class="contact--sec5--div15">
+                                <table>
+                                    <tr>
+                                        <td class="table--img1"><img src="../resources/img/contact/icon3.png" class="table--img1" /></td>
+                                        <td class="table--span2">Location</td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="contact--sec5--div16">
+                                <span class="sec5--iconspan11">+Rolleston, Christchurch, 7615,
+                                    New Zealand</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact--sec5--div18">
+                        <div class="contact--sec5--div14" style="background-color: #ECF4E0;">
+                            <div class="contact--sec5--div15">
+                                <table>
+                                    <tr>
+                                        <td class="table--img1"><img src="../resources/img/contact/icon4.png" class="table--img1" /></td>
+                                        <td class="table--span2">WhatsApp</td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="contact--sec5--div16">
+                                <span class="sec5--iconspan1">+64 27 329 6864</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                    <div class="contact--sec5--div21">
+                        <div class="contact--sec5--div19">
+                            <div class="contact--sec5--div14" style="background-color: #4D6D1D;">
+                                <div class="contact--sec5--div15">
+                                    <table>
+                                        <tr>
+                                            <td><img src="../resources/img/contact/icon1.png" class="table--img1" /></td>
+                                            <td class="table--span1">Contact Number</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="contact--sec5--div16">
+                                    <span class="sec5--iconspan">+64 27 329 6864</span>
+                                </div>
+                            </div>
+                            <div class="contact--sec5--div14" style="background-color: #669127;">
+                                <div class="contact--sec5--div15">
+                                    <table>
+                                        <tr>
+                                            <td class="table--img1"><img src="../resources/img/contact/icon2.png" class="table--img1" /></td>
+                                            <td class="table--span1">Email Address</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="contact--sec5--div16">
+                                    <span class="sec5--iconspan">info@skilly.co.nz</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="contact--sec5--div20">
+                            <div class="contact--sec5--div14" style="background-color: #80B531;">
+                                <div class="contact--sec5--div15">
+                                    <table>
+                                        <tr>
+                                            <td class="table--img1"><img src="../resources/img/contact/icon3.png" class="table--img1" /></td>
+                                            <td class="table--span2">Location</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="contact--sec5--div16">
+                                    <span class="sec5--iconspan11">Rolleston, Christchurch, 7615,
+                                        New Zealand</span>
+                                </div>
+                            </div>
+                            <div class="contact--sec5--div14" style="background-color: #ECF4E0;">
+                                <div class="contact--sec5--div15">
+                                    <table>
+                                        <tr>
+                                            <td class="table--img1"><img src="../resources/img/contact/icon4.png" class="table--img1" /></td>
+                                            <td class="table--span2">WhatsApp</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="contact--sec5--div16">
+                                    <span class="sec5--iconspan1">+64 27 329 6864</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="contact--sec5--div24">
+                <div class="contact--sec5--div25">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3968.3166521328867!2d80.5563884!3d5.951033300000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae139005828311b%3A0xdd0e4cb077063998!2sKHK%20Tours!5e0!3m2!1sen!2slk!4v1736124128896!5m2!1sen!2slk" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="contact--map--img"></iframe>
+                </div>
+            </div>
+
+
+
+
+
 
 
     <!-- Footer -->
