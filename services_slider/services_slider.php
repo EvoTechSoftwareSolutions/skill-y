9bc8743b0b0669ee5e1310e956e56627b41a4232
Chadina Lakshan <chadina.evotech@gmail.com>
2025-02-15 17:10:09 +0530

    chadina

--- services_slider/services_slider.php
+++ services_slider/services_slider.php
@@ -19,12 +19,11 @@
       <li class="card">
         <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
         <div class="home--sec3--div6">
-          <span class="home--sec3--span4">Collaboration Services to Institutions</span>
+          <span class="home--sec3--span4">Visiting Visas for Parents</span>
         </div>
         <div class="home--sec3--div7">
           <span class="home--sec3--span5">
-            Collaboration services provide a pathway to work together with a wide range of other institutions to achieve
-            a shared goal.
+            We are happy to provide you guidance in appliying for and granting visitor visas for parents.
           </span>
         </div>
         <div class="home--sec3--div8">
@@ -39,12 +38,11 @@
       <li class="card">
         <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
         <div class="home--sec3--div6">
-          <span class="home--sec3--span4">Collaboration Services to Institutions</span>
+          <span class="home--sec3--span4">Visa Assistance</span>
         </div>
         <div class="home--sec3--div7">
           <span class="home--sec3--span5">
-            Collaboration services provide a pathway to work together with a wide range of other institutions to achieve
-            a shared goal.
+            Visa is a legal document that allows visa holder to enter the desired country with permission
           </span>
         </div>
         <div class="home--sec3--div8">
@@ -59,12 +57,11 @@
       <li class="card">
         <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
         <div class="home--sec3--div6">
-          <span class="home--sec3--span4">Collaboration Services to Institutions</span>
+          <span class="home--sec3--span4">University Selection</span>
         </div>
         <div class="home--sec3--div7">
           <span class="home--sec3--span5">
-            Collaboration services provide a pathway to work together with a wide range of other institutions to achieve
-            a shared goal.
+            It is important ro select a university that fulfils your educational goals and matches your qualifications
           </span>
         </div>
         <div class="home--sec3--div8">
@@ -79,12 +76,11 @@
       <li class="card">
         <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
         <div class="home--sec3--div6">
-          <span class="home--sec3--span4">Collaboration Services to Institutions</span>
+          <span class="home--sec3--span4">Student Airport Pickup & Assistance</span>
         </div>
         <div class="home--sec3--div7">
           <span class="home--sec3--span5">
-            Collaboration services provide a pathway to work together with a wide range of other institutions to achieve
-            a shared goal.
+            You are not alone in Finland on your first arrival.We are committed to welcoming you at the airport
           </span>
         </div>
         <div class="home--sec3--div8">
@@ -99,12 +95,11 @@
       <li class="card">
         <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
         <div class="home--sec3--div6">
-          <span class="home--sec3--span4">Collaboration Services to Institutions</span>
+          <span class="home--sec3--span4">Student Accommodation</span>
         </div>
         <div class="home--sec3--div7">
           <span class="home--sec3--span5">
-            Collaboration services provide a pathway to work together with a wide range of other institutions to achieve
-            a shared goal.
+            One of the most important considerations is the accommodation that suits your budget and lifestyle.
           </span>
         </div>
         <div class="home--sec3--div8">
@@ -119,12 +114,11 @@
       <li class="card">
         <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
         <div class="home--sec3--div6">
-          <span class="home--sec3--span4">Collaboration Services to Institutions</span>
+          <span class="home--sec3--span4">Scholarship Assistance</span>
         </div>
         <div class="home--sec3--div7">
           <span class="home--sec3--span5">
-            Collaboration services provide a pathway to work together with a wide range of other institutions to achieve
-            a shared goal.
+            Winning a fully or a partially funded Scholarship in your draem university will be an immense financial benefit.
           </span>
         </div>
         <div class="home--sec3--div8">
@@ -136,6 +130,127 @@
           </div>
         </div>
       </li>
+      <li class="card">
+        <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
+        <div class="home--sec3--div6">
+          <span class="home--sec3--span4">Pre Departure and Post Arrival Services</span>
+        </div>
+        <div class="home--sec3--div7">
+          <span class="home--sec3--span5">
+            We make sure the pre-departure process is eased and facilitated with our services.
+          </span>
+        </div>
+        <div class="home--sec3--div8">
+          <div class="home--sec3--div9">
+            <span class="home--sec3--span6">View More Details</span>
+          </div>
+          <div class="home--sec3--div10">
+            <img src="../resources/img/home/arrow.png" class="home--sec3--img1" />
+          </div>
+        </div>
+      </li>
+
+      <li class="card">
+        <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
+        <div class="home--sec3--div6">
+          <span class="home--sec3--span4">Offer & Admissions in Universities/Colleges</span>
+        </div>
+        <div class="home--sec3--div7">
+          <span class="home--sec3--span5">
+           We assist you to correctly apply to universities and colleges according to the admission requirements.
+          </span>
+        </div>
+        <div class="home--sec3--div8">
+          <div class="home--sec3--div9">
+            <span class="home--sec3--span6">View More Details</span>
+          </div>
+          <div class="home--sec3--div10">
+            <img src="../resources/img/home/arrow.png" class="home--sec3--img1" />
+          </div>
+        </div>
+      </li>
+
+      <li class="card">
+        <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
+        <div class="home--sec3--div6">
+          <span class="home--sec3--span4">Educational Counselling & Career Guidance</span>
+        </div>
+        <div class="home--sec3--div7">
+          <span class="home--sec3--span5">
+           Students who wish to pursue higher education are often confused when making the decision.
+          </span>
+        </div>
+        <div class="home--sec3--div8">
+          <div class="home--sec3--div9">
+            <span class="home--sec3--span6">View More Details</span>
+          </div>
+          <div class="home--sec3--div10">
+            <img src="../resources/img/home/arrow.png" class="home--sec3--img1" />
+          </div>
+        </div>
+      </li>
+
+      <li class="card">
+        <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
+        <div class="home--sec3--div6">
+          <span class="home--sec3--span4">Dependent Visas</span>
+        </div>
+        <div class="home--sec3--div7">
+          <span class="home--sec3--span5">
+          A dependents visa allows the dependents of primary applicants to accompany them.
+          </span>
+        </div>
+        <div class="home--sec3--div8">
+          <div class="home--sec3--div9">
+            <span class="home--sec3--span6">View More Details</span>
+          </div>
+          <div class="home--sec3--div10">
+            <img src="../resources/img/home/arrow.png" class="home--sec3--img1" />
+          </div>
+        </div>
+      </li>
+
+
+      <li class="card">
+        <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
+        <div class="home--sec3--div6">
+          <span class="home--sec3--span4">Course Selection</span>
+        </div>
+        <div class="home--sec3--div7">
+          <span class="home--sec3--span5">
+           We provide assistance in course selection.Wherever you are headed.You will find a course that can get you to your desire.
+          </span>
+        </div>
+        <div class="home--sec3--div8">
+          <div class="home--sec3--div9">
+            <span class="home--sec3--span6">View More Details</span>
+          </div>
+          <div class="home--sec3--div10">
+            <img src="../resources/img/home/arrow.png" class="home--sec3--img1" />
+          </div>
+        </div>
+      </li>
+
+      <li class="card">
+        <div class="services--slider--img"><img src="../resources/img/home/1.png" alt="img" draggable="false"></div>
+        <div class="home--sec3--div6">
+          <span class="home--sec3--span4">Collaboration Serice to institutions</span>
+        </div>
+        <div class="home--sec3--div7">
+          <span class="home--sec3--span5">
+            Collaboration services provide a pathway to work together with a wide range of other institutions.
+          </span>
+        </div>
+        <div class="home--sec3--div8">
+          <div class="home--sec3--div9">
+            <span class="home--sec3--span6">View More Details</span>
+          </div>
+          <div class="home--sec3--div10">
+            <img src="../resources/img/home/arrow.png" class="home--sec3--img1" />
+          </div>
+        </div>
+      </li>
+
     </ul>
     <!-- <i id="right" class="fa-solid fa-angle-right"></i> -->
   </div>
