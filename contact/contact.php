6d2aa4023bbc875a34897019be8512efd9e88853
Kawya Awarjana <kawya.evotech@gmail.com>
2025-02-15 17:43:31 +0530

    Kawya

---
+++ contact/contact.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skill-y</title>
+    <link rel="icon" href="../resources/img/" />
+    <link rel="stylesheet" href="../contact/contact.css">
+    <link rel="stylesheet" href="../header/header.css">
+    <link rel="stylesheet" href="../footer/footer.css">
+</head>
+
+<body>
+
+    <!-- Header -->
+    <?php include "../header/header.php"; ?>
+    <!-- Header -->
+
+    <div class="contact--bgimg">
+        <!-- <img src="../resources/img/contact/Rectangle2.png" class="contact--bg1" /> -->
+    </div>
+
+    <div class="contact--bgimg1">
+        <!-- <img src="../resources/img/contact/Group2.png" class="contact--bg2" /> -->
+    </div>
+
+    <section class="contact--sec1">
+        <div class="contact--sec1--div1">
+            <span class="contact--sec1--span1">Skill-y /<span class="contact--sec1--span2"> About Us</span></span>
+        </div>
+    </section>
+
+    <section class="contact--sec2">
+        <div class="contact--sec2--div1">
+            <span class="contact--sec2--span1">About Us</span>
+            <span class="contact--sec2--span2">Why Choose<span class="contact--sec2--span3"> Skill-y Visa Consultancy?</span></span>
+        </div>
+    </section>
+
+
+    <section class="contact--sec3">
+        <div class="contact--sec3--div1">
+            <div class="contact--sec3--div2">
+                <img src="../resources/img/contact/con_img1.png" class="contact--sec3--img" />
+            </div>
+            <div class="contact--sec3--div3">
+                <div class="contact--sec3--div4">
+                    <span class="contact--sec3--span1">
+                        Skilly New Zealand is a unified network of 16 state-owned Institutes of Technology and Polytechnics (ITPs) and nine Industry Training Organisations (ITOs), operating as a single national institution. We provide high-quality, industry-aligned education and training, equipping learners with practical skills and globally recognized qualifications across diverse fields like engineering, healthcare, business, trades, and IT. With a strong focus on innovation and collaboration, we bridge the gap between education and industry, ensuring our graduates are workforce-ready and prepared for future career success.
+                    </span>
+                </div>
+                <div class="contact--sec3--div5">
+                    <div class="contact--sec3--div6">
+                        <div class="contact--sec3--div7">
+                            <img src="../resources/img/contact/career.png" class="contact--sec3--img1" />
+                        </div>
+                        <div class="contact--sec3--div8">
+                            <span class="contact--sec3--span2">Career Guidance</span>
+                            <span class="contact--sec3--span3">Find your perfect path.</span>
+                        </div>
+                    </div>
+                    <div class="contact--sec3--div6">
+                        <div class="contact--sec3--div7">
+                            <img src="../resources/img/contact/admission.png" class="contact--sec3--img2" />
+                        </div>
+                        <div class="contact--sec3--div8">
+                            <span class="contact--sec3--span2">Admissions Help</span>
+                            <span class="contact--sec3--span3">Get into your dream university.</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="contact--sec3--div9">
+                    <a href="../resources/pdf/Skilly_International_Study_Guide.pdf" download class="contact--download--btn">
+                        <span class="contact--sec3--span4">DOWNLOAD THE DOCUMENT</span>
+                        <img src="../resources/img/contact/down_arrow.png" class="contact--sec3--img3" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact--sec4">
+        <div class="contact--sec4--div1">
+            <div class="contact--sec4--div2">
+                <span class="contact--sec4--span1">IN PARTNERSHIP WITH</span>
+            </div>
+            <div class="contact--sec4--div3">
+                <div class="contact--sec4--div4">
+                    <img src="../resources/img/contact/partner1.png" class="partner1--logo">
+                </div>
+                <div class="contact--sec4--div5">
+                    <img src="../resources/img/contact/partner2.png" class="partner2--logo">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Footer -->
+    <?php include "../footer/footer.php"; ?>
+    <!-- Footer -->
+
+    <script src="../header/header.js"></script>
+    <script src="../slider/slider.js"></script>
+    <script src="home.js"></script>
+    <script src="../contact_slider/contact_slider.js"></script>
+    <script src="../resources/swiper/swiper-bundle.min.js"></script>
+    <script src="../home/home.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <!--=============== SWIPER JS ===============-->
+    <script src="assets/js/swiper-bundle.min.js"></script>
+
+    <!--=============== MAIN JS ===============-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
